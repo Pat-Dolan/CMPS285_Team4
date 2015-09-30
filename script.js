fedd717aa741116f33d554eb4b9a43e48f087a88
clobbermouth <cory.clapp@selu.edu>
2015-09-29 22:10:59 -0500

    set location
    Added in location to javascript call fucntion on the map zoomed to
    correct level.

--- script.js
+++
@@ -1,9 +0,0 @@
-var map;
-require(["esri/map", "dojo/domReady!"], function(Map) {
-  map = new Map("mapDiv", {
-	  /*geo coordinates go here*/
-    center: [-3.404154999999946, 56.280513],
-    zoom: 8,
-    basemap: "streets"
-  });
-});
