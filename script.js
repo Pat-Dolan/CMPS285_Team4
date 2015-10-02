5c6c8fa36ae3f4919d2b6538fb16c9a44dce6872
ChaseBlank <chase.blank@selu.edu>
2015-10-01 20:46:36 -0500

    homebutton
    added some js  and css for a homebutton

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 var map;
-require(["esri/map", "dojo/domReady!"], function(Map) {
+var home;
+require(["esri/map","esri/dijit/HomeButton", "dojo/domReady!"], function(Map, HomeButton) {
   map = new Map("mapDiv", {
     center: [1.868956,50.9518855],
     zoom: 7,
     basemap: "streets"
   });
+  home = new HomeButton({
+    map: map
+  }, "HomeButton");
+  home.startup();
 });
+
