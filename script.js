9618a6527ab14f815216ccc52b94b2965272d141
ChaseBlank <chase.blank@selu.edu>
2015-10-02 05:56:08 -0500

    layers and toggle layers
    Well it's 6am. pulled an all nighter to get this done. pretty big
    commit. it isnt very pretty right now but it works, and i'm tired. there
    is a csv file that loads earthquake data (just for show, we will replace
    it later, i just needed data to see what was happening). Go ahead click
    on the word earthquake and watch first layer magically disappear!! (zoom
    out first tho.)

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 var map;
 var home;
-require(["esri/map","esri/dijit/HomeButton", "dojo/domReady!"], function(Map, HomeButton) {
+var csv;
+require(["esri/map",
+    "esri/dijit/HomeButton",
+    "esri/layers/CSVLayer",
+    "esri/symbols/PictureMarkerSymbol",
+    "esri/renderers/SimpleRenderer",
+    "esri/InfoTemplate",
+    "dojo/domReady!"
+], function(Map, HomeButton, CSVLayer, PictureMarkerSymbol, SimpleRenderer, InfoTemplate) {
   map = new Map("mapDiv", {
     center: [1.868956,50.9518855],
     zoom: 7,
     basemap: "streets"
+    //basemap: "gray" //I think this looks nice
   });
-  home = new HomeButton({
-    map: map
+  home = new HomeButton({map: map
   }, "HomeButton");
   home.startup();
+
+//this CSV file will be our own
+  csv = new CSVLayer("2.5_week.csv", {
+      copyright: "USGS.gov"
+  });
+  var marker = new PictureMarkerSymbol("markers/StaticIcon1.png", 20, 20);
+  var renderer = new SimpleRenderer(marker);
+  csv.setRenderer(renderer);
+  var template = new InfoTemplate("${type}", "${place}");
+  csv.setInfoTemplate(template);
+  map.addLayer(csv);
+});
+
+$(document).ready(function(){
+    $("#layer1").click(function(){
+        if (csv.visible == true){
+            csv.hide();
+        }else{
+            csv.show();
+        }
+    });
 });
 
