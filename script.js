6e47a051dedfd9c9ace9d41e04afbffdab742ea9
clobbermouth <cory.clapp@selu.edu>
2015-10-04 09:58:29 -0500

    add WebMap.html

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var map;
-require(["esri/map", "dojo/domReady!"], function(Map) {
-  map = new Map("mapDiv", {
-    center: [1.868956,50.9518855],
-    zoom: 7,
-    basemap: "streets"
-  });
+require(["esri/map", "esri/layers/FeatureLayer", "esri/dijit/Legend",
+		 "dojo/_base/array", "dojo/parser", "dijit/layout/BorderContainer",
+		 "dijit/layout/ContentPane", "dijit/layout/AccordionContainer",
+		 "dojo/domReady!"], 
+		 function(Map, FeatureLayer, Legend, arrayUtils, parser) {
+			parser.parse();
+			map = new Map("mapDiv", {
+			center: [1.868956,50.9518855],
+			zoom: 7,
+			basemap: "streets"
+		});
 });
