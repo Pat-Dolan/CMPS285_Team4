ead5b2a69c5e40efc15bae75b64d009ed16d5814
ChaseBlank <chase.blank@selu.edu>
2015-11-16 12:43:55 -0600

    sync problems resolved.
    deleted unnecessary files wile at it.  timeline fixed

--- script.js
+++ script.js
@@ -15,103 +15,155 @@ $.ajax({
     url: "data.json",
     async: false,
     success: function(data){jsonData = data}
-
-
 });
-
-console.log(jsonData);
-
-
-
-
 
 require(["esri/map",
     "esri/layers/FeatureLayer",
+    "esri/request",
+    "dojo/on",
+    "dojo/_base/array",
     "esri/tasks/FeatureSet",
+    "esri/geometry/Point",
+    "esri/SpatialReference",
+    "esri/graphic",
+    "esri/layers/GraphicsLayer",
+    "esri/symbols/SimpleMarkerSymbol",
     "esri/dijit/Search",
+    "esri/dijit/PopupTemplate",
     "esri/dijit/HomeButton",
-	"esri/dijit/BasemapLayer",//adds basemap to layer
+    "esri/dijit/BasemapLayer",//adds basemap to layer
     "esri/dijit/Basemap",
-	"esri/layers/CSVLayer",
+    "esri/layers/CSVLayer",
     "esri/symbols/PictureMarkerSymbol",
     "esri/renderers/SimpleRenderer",
     "esri/InfoTemplate",
     "dojo/domReady!"
-], function(Map, FeatureLayer , FeatureSet, Search, HomeButton, BasemapLayer, Basemap, CSVLayer, PictureMarkerSymbol, SimpleRenderer, InfoTemplate) {
-  map = new Map("mapDiv", {
-    center: [1.868956,50.9518855],
-    zoom: 3,
-    //basemap: "streets"
-    basemap: "gray" //I think this looks nice
-  });
-  home = new HomeButton({map: map
-  }, "HomeButton");
-  home.startup();
-
-   /* var myFeatureSet = new FeatureSet(jsonData);
-
-    console.log(myFeatureSet);
-
-    var mylayerDefinition = {
-        "geometryType": "esriGeomtryPoint",
-        "fields": [
-            {
-                "name" : "Name",
-                "type": "esriFieldTypeOID",
-                "alias" : "Name"
-            }
-        ]
-    };
-
-    console.log(mylayerDefinition);
+], function(Map, FeatureLayer , Request, on, array, FeatureSet, Point, SpatialReference, Graphic, GraphicsLayer, SimpleMarkerSymbol, Search, PopupTemplate, HomeButton, BasemapLayer, Basemap, CSVLayer, PictureMarkerSymbol, SimpleRenderer, InfoTemplate) {
+    map = new Map("mapDiv", {
+        center: [1.868956,50.9518855],
+        zoom: 3,
+        //basemap: "streets"
+        basemap: "gray" //I think this looks nice
+    });
+    home = new HomeButton({map: map
+    }, "HomeButton");
+    home.startup();
 
     var featureCollection = {
-        layerDefinition : mylayerDefinition,
-        featureSet : myFeatureSet
+        "layerDefinition": null,
+        "featureSet": {
+            "features": [],
+            "geometryType": "esriGeometryPoint"
+        }
+    };
+    featureCollection.layerDefinition = {
+        "geometryType": "esriGeometryPoint",
+        "objectIdField": "ObjectID",
+        "drawingInfo": {
+            "renderer": {
+                "type": "simple",
+                "symbol": {
+                    "type": "esriPMS",
+                    "url": "resources/markers/StaticIcon1.png",
+                    "contentType": "resources/markers/png",
+                    "width": 15,
+                    "height": 15
+                }
+            }
+        },
+        "fields": [{
+            "name": "ObjectID",
+            "alias": "ObjectID",
+            "type": "esriFieldTypeOID"
+        }, {
+            "name": "description",
+            "alias": "Description",
+            "type": "esriFieldTypeString"
+        }, {
+            "name": "title",
+            "alias": "Title",
+            "type": "esriFieldTypeString"
+        }]
     };
 
-    console.log(featureCollection);
-
     var featureLayer = new FeatureLayer(featureCollection,{
-        mode : FeatureLayer.MODE_ONDEMAND
-
+        id:"RuskinLayer",
+        mode: FeatureLayer.MODE_SNAPSHOT
     });
+	
+	map.on("load", function(){
+		console.log("made it to the load function");
+		//requestData();
+	});
+	map.addLayer([featureLayer]);
+	
+   /* function requestData(){
+		console.log("made it to request data");
+        var requestHandle = esriRequest({
+            url: "AllPoints.json",
+			handleAs: "json",
+			timeout: 0,			
+            callbackParamName: "callback"
+        });
+		console.log("made it to past the handleEvent");
+        requestHandle.then(requestSucceeded, requestFailed);
+		
+    }
 
-    map.addLayer(featureLayer);
+    function requestSucceeded(response, io) {
+        //loop through the items and add to the feature layer
+        var features = [];
+        array.forEach(response.features, function(item) {
+            var attr = {};
+			attr["description"] = item.description;
+            //pull in any additional attributes if required
 
-    console.log(featureLayer);
+            var geometry = new Point(item.geometry.coordinates[0], item.geometry.coordinates[1]);
+
+            var graphic = new Graphic(geometry);
+            graphic.setAttributes(attr);
+            features.push(graphic);
+        });
+		console.log("made it to the request success function");
+        featureLayer.applyEdits(features, null, null);
+    }
+
+    function requestFailed(error) {
+        console.log('failed');
+    }
+	*/
 
     /*basemap layer
-    var layer = new BasemapLayer({
-        url:"http:http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer"
-    });
-    var basemap = new Basemap({
-        layers:[layer],
-        title:"Ruskin's map",
-        //thumbnailUrl: to show the image in thumbnail form in the gallery
-    });
-    basemapGallery.add(basemap);*/
+     var layer = new BasemapLayer({
+     url:"http:http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer"
+     });
+     var basemap = new Basemap({
+     layers:[layer],
+     title:"Ruskin's map",
+     //thumbnailUrl: to show the image in thumbnail form in the gallery
+     });
+     basemapGallery.add(basemap);*/
 
 //this CSV file will be our own
-  layer1 = new CSVLayer("2.5_week.csv", {
-      copyright: "USGS.gov"
-  });
-  var marker = new PictureMarkerSymbol("resources/markers/StaticIcon1.png", 20, 20);
-  var renderer = new SimpleRenderer(marker);
-  layer1.setRenderer(renderer);
-  var template = new InfoTemplate("${type}", "${place}");
-  layer1.setInfoTemplate(template);
-  map.addLayer(layer1);
+    layer1 = new CSVLayer("2.5_week.csv", {
+        copyright: "USGS.gov"
+    });
+    var marker = new PictureMarkerSymbol("resources/markers/StaticIcon1.png", 20, 20);
+    var renderer = new SimpleRenderer(marker);
+    layer1.setRenderer(renderer);
+    var template = new InfoTemplate("${type}", "${place}");
+    layer1.setInfoTemplate(template);
+    map.addLayer(layer1);
 
 
-  layer2 = new CSVLayer("Ruskin_Stuff.csv", {  });
-  var marker2 = new PictureMarkerSymbol("resources/markers/StaticIcon2.png", 20, 20);
-  var renderer2 = new SimpleRenderer(marker2);
-  layer2.setRenderer(renderer2);
-  var template2 = new InfoTemplate("${type}", "${place}");
-  layer2.setInfoTemplate(template2);
-  map.addLayer(layer2);
-
+    /*layer2 = new CSVLayer("Ruskin_Stuff.csv", {  });
+     var marker2 = new PictureMarkerSymbol("resources/markers/StaticIcon2.png", 20, 20);
+     var renderer2 = new SimpleRenderer(marker2);
+     layer2.setRenderer(renderer2);
+     var template2 = new InfoTemplate("${type}", "${place}");
+     layer2.setInfoTemplate(template2);
+     map.addLayer(layer2);
+     */
 
 
     s = new Search({
@@ -124,38 +176,38 @@ require(["esri/map",
         zoomScale : 5000000
     }, "search");
 
-        var sources = s.get("sources");
+    var sources = s.get("sources");
 
-        sources.push({
+    sources.push({
 
-            CSVLayer: new CSVLayer("Ruskin_Stuff.csv", {  }),
-            infoTemplate: new InfoTemplate("${type}", "${place}"),
-            enableSuggestions: true,
-            placeholder: "Spain",
-            enableLabel: false,
-            searchFields: ["place"],
-            displayField: "place",
-            name: "Ruskin",
-            maxSuggestions: 2,
-            exactMatch: false
+        CSVLayer: new CSVLayer("Ruskin_Stuff.csv", {  }),
+        infoTemplate: new InfoTemplate("${type}", "${place}"),
+        enableSuggestions: true,
+        placeholder: "Spain",
+        enableLabel: false,
+        searchFields: ["place"],
+        displayField: "place",
+        name: "Ruskin",
+        maxSuggestions: 2,
+        exactMatch: false
 
-        });
-        //Set the sources above to the search widget
-        s.set("sources", sources);
+    });
+    //Set the sources above to the search widget
+    s.set("sources", sources);
 
     s.startup();
 
 });
 
-  /*
-  layer3 = new CSVLayer("", {  });
-  var marker3 = new PictureMarkerSymbol("resources/markers/StaticIcon1.png", 20, 20);
-  var renderer3 = new SimpleRenderer(marker3);
-  layer2.setRenderer(renderer3);
-  var template3 = new InfoTemplate("${type}", "${place}");
-  layer2.setInfoTemplate(template3);
-  map.addLayer(layer3);
-   */
+/*
+ layer3 = new CSVLayer("", {  });
+ var marker3 = new PictureMarkerSymbol("resources/markers/StaticIcon1.png", 20, 20);
+ var renderer3 = new SimpleRenderer(marker3);
+ layer2.setRenderer(renderer3);
+ var template3 = new InfoTemplate("${type}", "${place}");
+ layer2.setInfoTemplate(template3);
+ map.addLayer(layer3);
+ */
 
 
 
@@ -177,14 +229,14 @@ $(document).ready(function(){
     });
 
     /*
-    $("#layer3").click(function(){
-        if (layer3.visible == true){
-            layer3.hide();
-        }else{
-            layer3.show();
-        }
-    });
-    */
+     $("#layer3").click(function(){
+     if (layer3.visible == true){
+     layer3.hide();
+     }else{
+     layer3.show();
+     }
+     });
+     */
 
     $("#upArrow").mouseenter(function(){
         $("i").css("opacity",1);
@@ -194,7 +246,7 @@ $(document).ready(function(){
         $("i").css("opacity",.5);
     });
     $("#upArrow").click(function(){
-		$("i").toggleClass("fa-arrow-down");		
+        $("i").toggleClass("fa-arrow-down");
         $("#footer").toggleClass("opened");
     });
 
@@ -284,7 +336,7 @@ function createBoxes(container){
         att2.value = "box"+"-"+container + "-"+i;
         div.setAttributeNode(att);
         div.setAttributeNode(att2);
-        var node = document.createTextNode("Here is a new DIV.");
+        var node = document.createTextNode("Here is a new DIV." + "box"+"-"+container + "-"+i);
         div.appendChild(node);
         element.appendChild(div);
         var elementbox = document.getElementById("box"+"-"+container + "-"+i);
